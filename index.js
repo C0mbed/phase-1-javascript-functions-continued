e50b16673429f108a650b0485bb861c63b711969
C0mbed <matt@farncombe.co>
2021-12-19 12:00:10 -0700

    commit tree 1ea543a565224175cab804f29db1f5dab53ed534

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+
+function saturdayFun(funThing) {
+    if (funThing) {
+        return "This Saturday, I want to " + funThing + "!";
+    } else {
+        return "This Saturday, I want to roller-skate!";
+    } 
+}
+
+function mondayWork(workThing) {
+    if (workThing) {
+        return "This Monday, I will " + workThing + ".";
+    } else {
+        return "This Monday, I will go to the office."
+    }
+}
+
+function wrapAdjective(thing) {
+    return function (str) {
+        return `You are ${thing}${str}${thing}!`;
+    };
+}
